02716905039b95f548fe1859c7d3c2843efdba80
XantX <hp_xantx@hotmail.com>
2022-12-18 12:48:04 -0500

    Actualizando configuracion de colores

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -3,8 +3,9 @@ function ColorMyPencil(color)
     --rose-pine
 	color = color or "gruvbox"
 	vim.cmd.colorscheme(color)
-	--vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	--vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+	vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
 end
 --vim.cmd([[colorscheme gruvbox]])
 ColorMyPencil("rose-pine")
