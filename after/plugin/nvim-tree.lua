5ed79a7e4eae743b2c5949b9aa4c8464de20f5b9
XantX <hp_xantx@hotmail.com>
2023-01-12 21:10:30 -0500

    Agregada configuracion de cursorline y del explorador de archivos

--- after/plugin/nvim-tree.lua
+++ after/plugin/nvim-tree.lua
@@ -4,12 +4,16 @@ vim.g.loaded_netrwPlugin = 1
 -- OR setup with some options
 require("nvim-tree").setup({
   sort_by = "case_sensitive",
+  auto_reload_on_write = true,
   disable_netrw = true,
   view = {
     adaptive_size = false,
+    cursorline = true,
+    preserve_window_proportions = true,
   },
   renderer = {
-    group_empty = true,
+    add_trailing = true,
+    group_empty = false,
   },
   filters = {
     dotfiles = true,
