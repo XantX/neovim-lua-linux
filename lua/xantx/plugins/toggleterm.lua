06ead2c3cbbcc4665b9aca884c25f2211e4872fa
XantX <hp_xantx@hotmail.com>
2024-01-26 21:00:26 -0500

    Configuration de toggleterm

---
+++ lua/xantx/plugins/toggleterm.lua
@@ -0,0 +1,28 @@
+return {
+  'akinsho/toggleterm.nvim',
+  version = '*',
+  opts = { 
+    open_mapping = [[<c-\>]],
+    direction = 'horizontal',
+    float_opts = {
+      border = "curved",
+    },
+  },
+  keys = { 
+    {
+    "<leader>th",
+    "<cmd>ToggleTerm direction=horizontal <CR>",
+    desc = "Open terminal"
+    },
+    {
+    "<leader>tv",
+    "<cmd>ToggleTerm direction=vertical <CR>",
+    desc = "Open terminal"
+    },
+    {
+    "<leader>tf",
+    "<cmd>ToggleTerm direction=float <CR>",
+    desc = "Open terminal"
+    }
+  }
+}
